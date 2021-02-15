1f4ae354376c0e9c998848cd68d6da175e38be0f
ElLuchoMan <luchoman96@gmail.com>
2021-02-14 21:06:14 -0500

    Agregados inputs dinámicos

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+interface Persona {
+  nombre: string,
+  favoritos: Favorito[],
+}
+interface Favorito {
+  id: number,
+  nombre: string,
+}
 
 @Component({
   selector: 'app-dinamicos',
@@ -6,11 +15,30 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class DinamicosComponent {
+  nuevoJuego: string = '';
+  persona: Persona = {
+    nombre: 'Diana Garzón',
+    favoritos: [
+      { id: 1, nombre: 'Mario Bross' },
+      { id: 2, nombre: 'Leap Ray' },
+      { id: 3, nombre: 'Call of Duty Mobile' }
+    ]
   }
+  guardar() {
+    console.log('Formulario posteado');
+  }
+  agregarJuego() {
+    const nuevoFavorito: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego,
+    }
+    this.persona.favoritos.push({...nuevoFavorito});
+    this.nuevoJuego='';
+  }
+  eliminar(index: number) {
+    this.persona.favoritos.splice(index, 1);
+  }
+
 
 }
