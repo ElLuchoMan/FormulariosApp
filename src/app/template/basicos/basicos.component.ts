f2c83719f028c448c64c0e3f5e838fe55c9542af
ElLuchoMan <luchoman96@gmail.com>
2021-02-14 19:46:49 -0500

    Agregada directiva personalizada

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -8,8 +8,15 @@ import { NgForm } from '@angular/forms';
   ]
 })
 export class BasicosComponent {
+  @ViewChild('miFormulario') miFormulario !: NgForm;
 
-guardar(miFormulario:NgForm){
-  console.log(miFormulario.value);
-}
+  guardar() {
+    console.log(this.miFormulario.value);
+  }
+  nombreValido(): boolean {
+    return this.miFormulario?.controls.producto?.invalid && this.miFormulario?.controls.producto?.touched;
+  }
+  precioValido(): boolean {
+    return this.miFormulario?.controls.precio?.value < 0 && this.miFormulario?.controls.precio?.touched;
+  }
 }
--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -6,10 +6,16 @@ import { BasicosComponent } from './basicos/basicos.component';
 import { DinamicosComponent } from './dinamicos/dinamicos.component';
 import { SwitchesComponent } from './switches/switches.component';
 import { FormsModule } from '@angular/forms';
+import { CustomMinDirective } from './directives/custom-min.directive';
 
 
 @NgModule({
-  declarations: [BasicosComponent, DinamicosComponent, SwitchesComponent],
+  declarations: [
+    BasicosComponent,
+     DinamicosComponent,
+      SwitchesComponent,
+      CustomMinDirective
+    ],
   imports: [
     CommonModule,
     TemplateRoutingModule,
