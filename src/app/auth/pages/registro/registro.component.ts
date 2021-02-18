4498236b66b116a2ae3a70b864b1cb1d39d4d33e
ElLuchoMan <luchoman96@gmail.com>
2021-02-17 21:35:23 -0500

    Agregadas las condiciones de error del email

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { emailPattern, nombreApellidoPattern, noPuedeSerLuchoman } from 'src/app/shared/validator/validaciones';
 import { ValidatorService } from '../../../shared/validator/validator.service';
+import { EmailValidatorService } from '../../../shared/validator/email-validator.service';
 
 @Component({
   selector: 'app-registro',
@@ -12,19 +13,33 @@ import { ValidatorService } from '../../../shared/validator/validator.service';
 export class RegistroComponent implements OnInit {
   miFormulario: FormGroup = this.fb.group({
     nombre: ['', [Validators.required, Validators.pattern(this.validatorService.nombreApellidoPattern)]],
-    email: ['', [Validators.required, Validators.pattern(this.validatorService.emailPattern)]],
+    email: ['', [Validators.required, Validators.pattern(this.validatorService.emailPattern)], [this.emailValidator]],
     username: ['', [Validators.required, this.validatorService.noPuedeSerLuchoman]],
     password: ['', [Validators.required, Validators.minLength(6)]],
     password2: ['', [Validators.required]],
   }, {
     validators: [this.validatorService.camposIguales('password', 'password2')]
   })
-  constructor(private fb: FormBuilder, private validatorService: ValidatorService) { }
+  get emailErrorMsg(): string {
+    const errors = this.miFormulario.get('email')?.errors;
+    if (errors?.required) {
+      return 'El email es obligatorio';
+    } else if (errors?.pattern) {
+      return 'El valor ingresado no tiene formato de correo';
+    } else if (errors?.emailTomado) {
+      return 'El correo ya está resgistrado';
+    }
+    return ''
+  }
+
+  constructor(private fb: FormBuilder, private validatorService: ValidatorService, private emailValidator: EmailValidatorService) { }
   ngOnInit(): void {
     this.miFormulario.reset({
       nombre: 'Bryan Luis',
       email: 'luchoman96@gmail.com',
-      username: 'Luchoman'
+      username: 'Luchoman',
+      password: '123456',
+      password2: '123456',
     });
   }
   campoNoValido(campo: string) {
@@ -33,4 +48,6 @@ export class RegistroComponent implements OnInit {
   submitFormulario() {
     this.miFormulario.markAllAsTouched();
   }
+
+
 }
